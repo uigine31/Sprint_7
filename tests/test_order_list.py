5dd78a5f315099b2f132e44ca23844109e29bcde
Evgeniy Smolovyi <uigine31@gmail.com>
2025-06-11 18:19:53 +0700

    Добавление allure.step аннотаций в test_order_create.py и test_order_list.py

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -9,6 +9,7 @@ class TestOrderList:
 
     @allure.title('Получение списка заказов')
     def test_get_order_list(self):
-        response = requests.get(self.BASE_URL)
+        with allure.step("Отправка запроса на получение списка заказов"):
+            response = requests.get(self.BASE_URL)
         assert response.status_code == 200
         assert "orders" in response.json()
