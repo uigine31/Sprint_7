e2c8890365ed325cfa96bcac0b474c2f3290a1b7
Evgeniy Smolovyi <uigine31@gmail.com>
2025-06-11 17:47:22 +0700

    Исправление API (409, 'Этот логин уже используется'

--- tests/test_courier_create.py
+++ tests/test_courier_create.py
@@ -2,10 +2,21 @@ import requests
 import pytest
 import allure
 from urls import BASE_URL, COURIER_ENDPOINT
+from utils.courier_utils import delete_courier, login_courier, register_new_courier_and_return_login_password
 
 @allure.feature('Courier Creation')
 class TestCourierCreate:
     BASE_URL = f"{BASE_URL}{COURIER_ENDPOINT}"
+
+    @pytest.fixture
+    def courier(self):
+        login_pass = register_new_courier_and_return_login_password()
+        assert len(login_pass) == 3, "Курьер не создан"
+        login, password, _ = login_pass
+        courier_id = login_courier(login, password)
+        yield login, password
+        if courier_id:
+            delete_courier(courier_id)
 
     @allure.title('Успешное создание курьера')
     def test_create_courier_success(self, courier):
@@ -13,7 +24,7 @@ class TestCourierCreate:
         payload = {"login": login, "password": password, "firstName": "Test"}
         response = requests.post(self.BASE_URL, json=payload)
         assert response.status_code == 409
-        assert "Этот логин уже используется. Попробуйте другой." in response.json().get("message", "")
+        assert "Этот логин уже используется" in response.json().get("message", "")
 
     @allure.title('Нельзя создать двух одинаковых курьеров')
     def test_create_duplicate_courier_fails(self, courier):
@@ -21,7 +32,7 @@ class TestCourierCreate:
         payload = {"login": login, "password": password, "firstName": "Test"}
         response = requests.post(self.BASE_URL, json=payload)
         assert response.status_code == 409
-        assert "Этот логин уже используется. Попробуйте другой." in response.json().get("message", "")
+        assert "Этот логин уже используется" in response.json().get("message", "")
 
     @allure.title('Все обязательные поля нужны для создания курьера')
     def test_create_courier_missing_fields_fails(self, courier):
